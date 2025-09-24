e3cec45a0c93c1677318464d2f365c4b4d92bf27
Walker Aldridge <daldridge@lairdscomputer.com>
2025-09-24 13:08:39 -0500

    Add deeply nested for clock and set of sets

--- samples/for_clock.py
+++ samples/for_clock.py
@@ -5,10 +5,12 @@ print()
 for hour in range(0,23) {
     for minute in range(0,60) {
         for second in range(0,60) {
-            if (firsttick == False) {sys.stdout.write('\033[F')}  # Move cursor up one line
+            if (firsttick == False) {
+                sys.stdout.write('\033[F')
+                } # Move cursor up one line
             time.sleep(1)
             print(f"{hour}:{minute}:{second}")
             firsttick = False
+            }
         }
     }
-}
